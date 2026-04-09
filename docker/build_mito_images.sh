70d9b930ff5588ccb3e7f5e0663b78f91ab23944
root <root@BGS-STU22-170.pmacs.upenn.edu>
2026-04-09 12:33:44 -0400

    Removed docker

--- docker/build_mito_images.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
-
-docker build -t pipevar_mito/mito_mutect2:0.1.0 "${ROOT_DIR}/docker/mito_mutect2"
-docker build -t pipevar_mito/mito_annotation:0.1.0 "${ROOT_DIR}/docker/mito_annotation"
